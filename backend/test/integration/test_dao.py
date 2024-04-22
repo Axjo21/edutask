6c126dc7dface910d9c7e9b92695de5fee29f034
Axjo21 <axjo21@student.bth.se>
2024-04-22 21:09:03 +0200

    first commit for integration tests

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -1,7 +1,25 @@
 import pytest
 
-"""
+from unittest.mock import patch
+
+from src.util.dao import DAO
+
+@patch('src.util.dao.getValidator', autospec=True)
 @pytest.fixture
-def mongodb():
+def sut():
     
-"""
+    mocked_dao = DAO("new_test_collection")
+
+    yield mocked_dao
+
+    # create DAO
+    # mock hard-coded dependencies?
+
+    # setup collection
+    # setup validator
+
+    # setup done -> start testing create method
+
+    # yield
+
+    # tear down / cleanup collection
